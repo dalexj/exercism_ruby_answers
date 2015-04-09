6dfc25a0daa707c1a088d2cddf702cfd4df43176
Alex <96dalex@gmail.com>
2015-04-09 14:52:36 -0600

    refactored roman numerals so hash isnt relying on being sorted

--- roman-numerals/roman.rb
+++ roman-numerals/roman.rb
@@ -1,17 +1,18 @@
-# class Fixnum
-#   def to_roman
-#     number = self
-#     roman_values = {M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1}
-#
-#     roman_values.keys.reduce("") do |numerals, key|
-#       numeral_amount, number = number.divmod roman_values[key]
-#       numerals + key.to_s * numeral_amount
-#     end
-#   end
-# end
-require 'roman-numerals'
 class Fixnum
+  ROMAN_CONVERSION = {
+    M:1000, CM:900, D:500, CD:400, C:100, XC:90,
+    L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1
+  }
+
   def to_roman
-    RomanNumerals.to_roman(self)
+    number = self
+    sorted_roman_numerals.reduce("") do |numerals, key|
+      numeral_amount, number = number.divmod ROMAN_CONVERSION[key]
+      numerals + key.to_s * numeral_amount
+    end
+  end
+
+  def sorted_roman_numerals
+    ROMAN_CONVERSION.keys.sort_by { |numeral| -ROMAN_CONVERSION[numeral] }
   end
 end
