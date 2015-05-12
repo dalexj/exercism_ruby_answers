b1e68bd887503e5e9fc9e3b8478f2610371389b2
Alex <96dalex@gmail.com>
2015-05-12 02:48:58 -0600

    hello world 2

--- hello-world/hello_world.rb
+++ hello-world/hello_world.rb
@@ -1,6 +1,38 @@
 class HelloWorld
-  def self.hello(name)
-    name = "world" if name.empty?
-    "Hello, #{name}!"
+  def self.hello(name = nil)
+    greet set_name_if_doesnt_exist(name)
+  end
+
+  def self.set_name_if_doesnt_exist(name, default = "world")
+    (name && name.empty?) ? default : name
+  end
+
+  def self.greet(name)
+    "#{GreetingCapitalizer.capital_default}, #{name}!"
+  end
+end
+
+class GreetingCapitalizer
+  def self.capital_default
+    Greetings.default.capitalize
+  end
+end
+
+class Greetings
+  def self.default
+    all.first
+  end
+
+  def self.all
+    [
+      "hello",
+      "hi",
+      "sup",
+      "hey",
+      "yo",
+      "good morning",
+      "good afternoon",
+      "good evening",
+    ]
   end
 end
